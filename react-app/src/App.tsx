b268f85496deedcc7a351c45fbe2f3cc1a7d8020
Srivatsav Chinta <srivatsavchinta@Srivatsavs-Air.lan>
2024-11-16 15:47:54 +0530

    added button component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,18 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
